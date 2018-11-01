fc278b9df3f11d3719110dc2ab5d78faafedaaaf
MashiroShina <lshinamashiro@gmail.com>
2018-11-01 21:26:37 +0900

    Initial commit

---
+++ Assets/RotationTransformation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RotationTransformation : Transformation
+{
+    public Vector3 rotation;
+    public override Vector3 Apply(Vector3 point)
+    {
+        float radzZ = rotation.z * Mathf.Deg2Rad;
+        float sinZ = Mathf.Sin(radzZ);
+        float cosZ = Mathf.Cos(radzZ);
+        return new Vector3(point.x * cosZ - point.y * sinZ,
+            point.x * sinZ + point.y * cosZ,
+            point.z);
+        //return new Vector3(point.x*cosZ,point.y*sinZ,point.z);
+    }
+}
